2371d0bff2c45496f0e5d6d8595235b9356162e9
Jeremiah <jayonojah@gmail.com>
2024-09-24 06:50:23 +0100

    Authentication Completion

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Medal } from "lucide-react";
 import localFont from "next/font/local";
-import { Poppins } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -10,7 +10,7 @@ const headingFont = localFont({
   src: "../../public/fonts/font.woff2",
 });
 
-const textFont = Poppins({
+const textFont = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
@@ -25,10 +25,10 @@ const LandingPage = () => {
         )}>
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="size-6 mr-2" />
-          No 1 task management
+          <span className="word-spacing">#1 Task Management Platform</span>
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-          TaskMate helps team move
+          TaskMate helps teams advance.
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
           Work Forward.
@@ -36,7 +36,7 @@ const LandingPage = () => {
       </div>
       <div
         className={cn(
-          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          "text-sm md:text-xl text-neutral-600 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
           textFont.className
         )}>
         Collaborate, manage projects, and reach new productivity peaks. From
