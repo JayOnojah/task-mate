ac0f96f805f1b1b139c484c8ca33a86d9ee0080d
Jeremiah <jayonojah@gmail.com>
2024-09-24 06:13:17 +0100

    Authentication Set-up Complete

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "TaskMate",
+  description: "A task management app",
+  url: "https://taskmate.onojah.com",
+  ogImage: "https://taskmate.shadcn.com/og.png",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+  },
+};
